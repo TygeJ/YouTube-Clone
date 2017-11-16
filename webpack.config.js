31c0a3c112f31e547542a7bbcef1fd487f5bf80f
Tyge <tygesjohnso@gmail.com>
2017-11-16 15:58:55 -0700

    yer mamma

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
+const path = require('path');
+
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
   },
   module: {
     loaders: [{
